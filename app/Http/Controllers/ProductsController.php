036287fb5e7638c92b824fc15adacce60ee03d0b
Azwin Azman <azwin2263@gmail.com>
2025-12-20 16:34:00 +0800

    Third commit: completed

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -11,6 +11,7 @@ class ProductsController extends Controller
     public function index() {
         $products = Product::all();
         return response()->json($products,200);
+        //return response()->json(['message' => 'Connected!']);
     }
 
     public function store(Request $request) {
@@ -26,7 +27,48 @@ class ProductsController extends Controller
 
         return response()->json(
             ['success' => true,
-             'data' => $product]
+             'data' => $product], 201
+        );
+    }
+
+    public function update(Request $request, $id) {
+        $product = Product::findOrFail($id);
+
+        $validated = $request->validate(
+            ['name' => 'string',
+             'description' => 'nullable|string',
+             'price' => 'required|numeric|decimal:0,2|between:0,99999999.99',
+             'stock' => 'integer'
+              ]
+        );
+
+        $product->update($validated);
+
+        return response()->json(
+            ['success' => true,
+             'data' => $product],200
+        );
+    }
+
+    public function show($id) {
+        $product = Product::findOrFail($id);
+
+        return response()->json($product,200);
+    }
+
+    public function destroy($id) {
+        $product = Product::find($id);
+
+        if (!$product) {
+            return response()->json(
+                ['message' => 'Product not found.'], 404
+            );
+        }
+
+        $product->delete();
+
+        return response()->json(
+            ['message' => 'Product deleted successfully.'], 200
         );
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductsController;
 
 //Route::get('notes',[NotesController::class,'index']);
-Route::get('products',[ProductsController::class,'index']);
+Route::get('/products',[ProductsController::class,'index']);
 Route::post('products',[ProductsController::class,'store']);
+Route::put('products/{id}',[ProductsController::class,'update']);
+Route::get('products/{id}',[ProductsController::class,'show']);
+Route::delete('products/{id}',[ProductsController::class,'destroy']);

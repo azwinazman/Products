131a17401fe31fe979b43bb4aedfe289dd9678f1
Azwin Azman <azwin2263@gmail.com>
2026-01-04 16:54:30 +0800

    Third commit: After completing E2: Authentication

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Sanctum\HasApiTokens;
+
+class AuthController extends Controller
+{
+    //
+    public function login(Request $request) {
+        $request->validate(
+            [ 'email' => 'required|email',
+              'password' => 'required']
+        );
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json('User is not authenticated');
+        }
+
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json(
+            [ 'user' => $user,
+            'token' => $token ], 200
+        );
+    }
+
+    public function store(Request $request) {
+        $validated = $request->validate(
+            [ 'name' => 'required|string|max:255',
+              'email' => 'required|string|email|unique:users',
+              'password' => [
+                    'required',
+                    Password::min(8)
+                        ->letters()
+                        ->mixedCase()
+                        ->numbers()
+                        ->symbols()
+              ] 
+            ]
+        );
+
+        $user = User::create(
+            [ 'name' => $validated['name'],
+              'email' => $validated['email'],
+              'password' => Hash::make($validated['password']) ] 
+        );
+
+
+        return response()->json(
+            [ 'message' => 'User registered successfully!',
+              'success' => true,
+              'data' => $user], 201
+        );
+    }
+
+    public function logout(Request $request)
+    {
+        $user = Auth::user();
+        // Revoke (delete) the token that was used to authenticate the current request
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Successfully logged out',
+            'success' => true,
+            'data' => $user
+        ], 200);
+    }
+
+    public function me() {
+        $user = Auth::user();
+
+        return response()->json(
+            [ 'success' => true,
+              'data' => $user], 200 
+        );
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\Auth\AuthController;
 
 //Route::get('notes',[NotesController::class,'index']);
 Route::get('/products',[ProductsController::class,'index']);
@@ -9,3 +10,8 @@ Route::post('products',[ProductsController::class,'store']);
 Route::put('products/{id}',[ProductsController::class,'update']);
 Route::get('products/{id}',[ProductsController::class,'show']);
 Route::delete('products/{id}',[ProductsController::class,'destroy']);
+
+Route::post('auth/register',[AuthController::class,'store']);
+Route::post('auth/login',[AuthController::class,'login']);
+Route::post('auth/logout',[AuthController::class,'logout'])->middleware('auth:sanctum');
+Route::get('auth/me',[AuthController::class,'me'])->middleware('auth:sanctum');
